80a89704021dae2198baded4be6250526fd3fc12
Hassan Jamot <jamothassan675@gmail.com>
2025-05-02 18:32:25 -0500

    card style review

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,9 +2,9 @@ import "./ItemCard.css";
 
 export default function ItemCard({ item }) {
   return (
-    <div className="cards">
+    <ul className="cards">
       <h2 className="cards__title">{item.name}</h2>
       <img src={item.link} alt={item.name} className="cards__image" />
-    </div>
+    </ul>
   );
 }
